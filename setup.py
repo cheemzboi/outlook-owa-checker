3482df17bf49c57bc7114c5cf27baed9c0dbe924
cheemzboi <opskb@opskb.xyz>
2022-09-19 15:08:15 +0530

    added exe in  dist

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+import requests
+#from bs4 import BeautifulSoup as bs
+import csv 
+import re 
+from distutils.core import setup # Need this to handle modules
+import py2exe 
+#latest1
+
+
+setup(windows=['owa.py'])
